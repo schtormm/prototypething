16f4ef2fc3cfc1e0626cdf5e26b49c3be53ad40a
schtormm <storm.noordermeer@gmail.com>
2024-01-16 18:15:36 +0100

    efksjjf

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 goOff = False
-actual_time = 0;
-Display: grove.TM1637 = None
-Display = grove.create_display(DigitalPin.P0, DigitalPin.P14)
+actual_time = 0
+_4digit = grove.create_display(DigitalPin.P0, DigitalPin.P14)
 timeanddate.set24_hour_time(12, 25, 0)
 def on_forever():
-    global _4digit
+    global actual_time
     
     if goOff == True:
         pass
     def onNumeric_time(hour, minute, second, month, day, year):
-        actual_time = parse_int(str(hour) + str(minute))
+        actual_time = int(str(hour) + str(minute))
     timeanddate.numeric_time(onNumeric_time)
     
-    Display.show(actual_time)
-    basic.pause(5000)
+    _4digit.show()
+    basic.pause(500)
     timeanddate.advance_by(-1, timeanddate.TimeUnit.SECONDS)
 basic.forever(on_forever)
 
