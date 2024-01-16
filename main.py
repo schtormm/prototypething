2ebb386b8ddeef4135de43d4fd6f35a3f9b4791a
schtormm <storm.noordermeer@gmail.com>
2024-01-16 18:33:19 +0100

    eindelijk werkende

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
-goOff = False
+def on_pin_pressed_p1():
+    basic.show_number(actual_time)
+input.on_pin_pressed(TouchPin.P1, on_pin_pressed_p1)
+
 actual_time = 0
 _4digit = grove.create_display(DigitalPin.P0, DigitalPin.P14)
-timeanddate.set24_hour_time(12, 25, 0)
-def on_forever():
-    global actual_time
-    
-    if goOff == True:
-        pass
-    def onNumeric_time(hour, minute, second, month, day, year):
-        actual_time = int(str(hour) + str(minute))
-    timeanddate.numeric_time(onNumeric_time)
-    
-    _4digit.show()
-    basic.pause(500)
-    timeanddate.advance_by(-1, timeanddate.TimeUnit.SECONDS)
-basic.forever(on_forever)
+timeanddate.set_time(12, 25, 0, timeanddate.MornNight.PM)
 
-def on_pin_pressed_p1():
-    global goOf
-    goOff = True
-input.on_pin_pressed(TouchPin.P1, on_pin_pressed_p1)
+def on_forever():
+    
+    def on_numeric_time(hour, minute, second, month, day, year):
+        global actual_time
+        actual_time2 = int("" + str(minute) + ("" + str(second)))
+    timeanddate.numeric_time(on_numeric_time)
+    
+    _4digit.show(actual_time)
+    basic.pause(500)
+    timeanddate.advance_by(0, timeanddate.TimeUnit.MILLISECONDS)
+basic.forever(on_forever)
